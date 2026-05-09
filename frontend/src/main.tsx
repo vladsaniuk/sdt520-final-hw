228f36cf031f0d0721cf8c7c81dcc875e5264e23
Vladimir Gleba <gleba.vladimir@gmail.com>
2026-05-09 22:04:58 +0300

    feat(02.1-01): install @fontsource/geist and wire globally; fix mermaid.css Inter override

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,3 +1,6 @@
+import '@fontsource/geist/400.css'
+import '@fontsource/geist/700.css'
+import '@fontsource/geist-mono/400.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
